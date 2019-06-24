24bdc7b74fd4bbbbeb174cd2f967ab34b3762b10
xav <loup.xav@gmail.com>
2019-06-24 09:36:54 +0200

    todo+carrousel(pasFini)

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,28 @@
 <template>
   <div id="app">
+    <carousel>
+      <carousel-slide>
+        SALUT
+        <img src="https://picsum.photos/id/215/200/300" alt>
+      </carousel-slide>
+      <carousel-slide>
+        AUREVOIR
+        <img src="https://picsum.photos/id/200/200/300" alt>
+      </carousel-slide>
+      <carousel-slide>
+        AUREVOIR
+        <img src="https://picsum.photos/id/210/200/300" alt>
+      </carousel-slide>
+      <carousel-slide>
+        AUREVOIR
+        <img src="https://picsum.photos/id/120/200/300" alt>
+      </carousel-slide>
+      <carousel-slide>
+        AUREVOIR
+        <img src="https://picsum.photos/id/320/200/300" alt>
+      </carousel-slide>
+    </carousel>
+
     <img src="./assets/logo.png">
     {{todosApp}}
     <button @click="addTodos">Ajouter todos</button>
@@ -7,21 +30,22 @@
     <todos v-model="todosApp"></todos>
     <h2>TODO1bis</h2>
     <todos v-model="todosApp"></todos>
-    <h2>TODO3</h2>
-    <todos></todos>
-    <h2>TODO3</h2>
-    <todos></todos>
   </div>
 </template>
 
 <script>
+import Carousel from "./components/carousel/Carousel.vue";
+import CarouselSlide from "./components/carousel/CarouselSlide.vue";
+
 import Todos from "./components/Todos.vue";
 import TodosVue from "./components/Todos.vue";
 
 export default {
   name: "App",
   components: {
-    Todos
+    Todos,
+    Carousel,
+    CarouselSlide
   },
   data() {
     return {
---
+++ src/components/carousel/Carousel.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <slot></slot>
+    <button class="carousel__nav carousel__prev" @click.prevent="prev">Précédent</button>
+    <button class="carousel__nav carousel__next" @click.prevent="next">Suivante</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      index: 0,
+      slides: []
+    };
+  },
+  mounted() {
+    this.slides = this.$children;
+    this.slides.forEach((slide, i) => {
+      slide.index = i;
+    });
+  },
+  computed: {
+    slidesCount() {
+      return this.slides.length;
+    }
+  },
+  methods: {
+    next() {
+      this.index++;
+      if (this.index >= this.slidesCount) {
+        this.index = 0;
+      }
+    },
+    prev() {
+      this.index--;
+      if (this.index < 0) {
+        this.index = this.slidesCount - 1;
+      }
+    }
+  }
+};
+</script>
+
+<style>
+.carousel__nav {
+  margin: 10px;
+  padding: 5px;
+}
+.carousel__next {
+  background-color: orange;
+}
+.carousel__prev {
+  background-color: pink;
+}
+</style>
